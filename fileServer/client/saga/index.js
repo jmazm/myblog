09fcba8bcb209e3211c22e0864f0af6c4988932b
jm <471938302@qq.com>
2018-05-02 21:43:08 +0800

    branch dev

--- fileServer/client/saga/index.js
+++
@@ -1,8 +0,0 @@
-import {fork} from 'redux-saga/effects'
-import {getAllImgsFlow} from './imgSaga'
-import {getAllFolderFlow} from './folderSaga'
-
-export default function* rootSage () {
-  yield fork(getAllImgsFlow)
-  yield fork(getAllFolderFlow)
-}
