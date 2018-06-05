04ccd2e1027799f501df85c14ae7c98993949c04
jm <471938302@qq.com>
2018-06-06 00:29:13 +0800

    prod

--- client/server/index.js
+++
@@ -1,5 +0,0 @@
-const createServer = require("./config")
-const common = require("../../config")
-const indexConfig = require("../config/webpack.index.dev.config")
-
-createServer(indexConfig, common.server.indexServerPort)
