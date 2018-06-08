445dd15769254f6e27950b825c388135f7d628ae
jm <471938302@qq.com>
2018-06-08 22:16:04 +0800

    prod

--- server/cmsServer.js
+++
@@ -1,8 +0,0 @@
-const app = require("./basicConfig")
-const common = require("../config")
-
-let port = common.server.cmsServerPort
-
-app.listen(port, () => {
-  console.log(`${process.version} listening on port ${port} (${app.env}/${common.dbConfig.database})`)
-})
--- server/router/user.js
+++
@@ -1,8 +0,0 @@
-const Router = require("koa-router")
-const userCtrl = require("../controllers/user")
-
-const router = new Router()
-
-router.post('/login', userCtrl.login)
-
-module.exports = router
