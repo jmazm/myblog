3b411cfb6945817bad7789dbe1f39f71e55ec52d
jm <471938302@qq.com>
2018-06-09 15:10:21 +0800

    prod

--- server/cmsServer.js
+++ server/cmsServer.js
@@ -1,8 +1,23 @@
+
+// 监听端口
+
 const app = require("./basicConfig")
-const common = require("../config")
+const https = require("https")
+const fs = require("fs")
+const config = require("../config")
+const port = config.server.cmsServerPort
 
-let port = common.server.cmsServerPort
 
-app.listen(port, () => {
-  console.log(`${process.version} listening on port ${port} (${app.env}/${common.dbConfig.database})`)
-})
+// app.listen(port, () => {
+//   console.log(`${process.version} listening on port ${port} (${app.env}/${common.dbConfig.database})`)
+// })
+
+
+const options = {
+  key: fs.readFileSync(config.auth.key),
+  cert: fs.readFileSync(config.auth.cert)
+}
+
+https.createServer(options, app.callback()).listen(port)
+
+console.log(`${process.version} listening on port ${port} (${app.env}/${config.dbConfig.database})`)
--- server/indexServer.js
+++ server/indexServer.js
@@ -1,12 +1,23 @@
 
 // 监听端口
-// https.createServer(config.https.options,app.callback()).listen(config.https.port)
 
 const app = require("./basicConfig")
-const common = require("../config")
+const https = require("https")
+const fs = require("fs")
+const config = require("../config")
+const port = config.server.indexServerPort
 
-let port = common.server.indexServerPort
 
-app.listen(port, () => {
-  console.log(`${process.version} listening on port ${port} (${app.env}/${common.dbConfig.database})`)
-})
+// app.listen(port, () => {
+//   console.log(`${process.version} listening on port ${port} (${app.env}/${common.dbConfig.database})`)
+// })
+
+
+const options = {
+  key: fs.readFileSync(config.auth.key),
+  cert: fs.readFileSync(config.auth.cert)
+}
+
+https.createServer(options, app.callback()).listen(port)
+
+console.log(`${process.version} listening on port ${port} (${app.env}/${config.dbConfig.database})`)
