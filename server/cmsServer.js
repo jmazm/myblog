626b9822d58bad3989eaf420c0f25cd8f8c5f22e
jm <471938302@qq.com>
2018-06-10 14:09:27 +0800

    dev

--- server/cmsServer.js
+++ server/cmsServer.js
@@ -1,23 +1,26 @@
-
-// 监听端口
-
+/**
+ * http
+ */
 const app = require("./basicConfig")
-const https = require("https")
-const fs = require("fs")
+// const https = require("https")
+// const fs = require("fs")
 const config = require("../config")
 const port = config.server.cmsServerPort
 
 
-// app.listen(port, () => {
-//   console.log(`${process.version} listening on port ${port} (${app.env}/${common.dbConfig.database})`)
-// })
+app.listen(port, () => {
+  console.log(`${process.version} listening on port ${port} (${app.env}/${config.dbConfig.database})`)
+})
 
+/**
+ * https
+ */
 
-const options = {
-  key: fs.readFileSync(config.auth.key),
-  cert: fs.readFileSync(config.auth.cert)
-}
-
-https.createServer(options, app.callback()).listen(port)
-
-console.log(`${process.version} listening on port ${port} (${app.env}/${config.dbConfig.database})`)
+// const options = {
+//   key: fs.readFileSync(config.auth.key),
+//   cert: fs.readFileSync(config.auth.cert)
+// }
+//
+// https.createServer(options, app.callback()).listen(port)
+//
+// console.log(`${process.version} listening on port ${port} (${app.env}/${config.dbConfig.database})`)
